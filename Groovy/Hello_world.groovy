410dc8c32a4d03c7a16eb601f5126cb696e86f57
ktsreddy007 <cloudtech047@gmail.com>
2024-02-12 22:48:55 +0530

    First Groovy

--- Groovy/Hello_world.groovy
+++ Groovy/Hello_world.groovy
@@ -2,5 +2,6 @@ class Example {
    static void main(String[] args) {
       // Using a simple println statement to print output to the console
       println('Hello World');
+      print ('Hello Teja')
    }
 }
