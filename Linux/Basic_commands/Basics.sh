a8f0bafa5f5ff8c2a2e6ab062b73e34091bd3981
ktsreddy007 <cloudtech047@gmail.com>
2024-09-02 15:10:54 +0530

    New Commands added

--- Linux/Basic_commands/Basics.sh
+++ Linux/Basic_commands/Basics.sh
@@ -1,2 +1,16 @@
 mkdir
 --Used for creating directory
+find / -name tej.txt
+--Used to find the particular file globally on all file directories present in linux but its shows only file visibilty not entire path.
+lolcate tej.txt
+--It will show full path of the searching file 
+========================================File System Navigation Commands=============================
+Cd /root 
+--It stands for Change Directory it helps to go to given target directory.
+pwd
+--It stands for Present Working Directory it helps to show the path of the directory we are present in.
+ls
+---It stands for listing , it helps to show the files / diectories list present.
+
+
+
